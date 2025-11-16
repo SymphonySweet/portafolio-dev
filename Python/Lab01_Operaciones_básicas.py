2be9d34b6549a1952ee57a20b85c8948c7a3e998
SymphonySweet <134564852+SymphonySweet@users.noreply.github.com>
2025-11-16 13:14:58 -0500

    Create Lab01_Operaciones_básicas.py

---
+++ Python/Lab01_Operaciones_básicas.py
@@ -0,0 +1,13 @@
+print("Operaciones básicas")
+nombre=input("Ingrese su Nombre: ")
+n1= int(input("Ingrese número 1:"))
+n2= int(input("Ingrese número 2:"))
+suma= n1+n2
+resta= n1-n2
+producto= n1*n2
+division= n1/n2
+print("¡Hola" , nombre, "!")
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La producto es: ", producto)
+print("La division es: ", division)
