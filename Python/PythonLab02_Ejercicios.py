d5fd592116328b3e2efd836b091ac256325a8901
SymphonySweet <134564852+SymphonySweet@users.noreply.github.com>
2025-11-16 13:19:38 -0500

    Create PythonLab02_Ejercicios.py

---
+++ Python/PythonLab02_Ejercicios.py
@@ -0,0 +1,35 @@
+import math
+numero = float(input("Ingrese un número: "))
+raiz_cubica = math.pow(numero, 1/3)
+print("La raíz cúbica es:", raiz_cubica)
+
+import math
+base = float(input("Ingrese la base: "))
+exponente = float(input("Ingrese el exponente: "))
+resultado = math.pow(base, exponente)
+print("El resultado es:", resultado)
+
+
+numerador = float(input("Ingrese el numerador: "))
+denominador = float(input("Ingrese el denominador: "))
+cociente = numerador / denominador
+print("El cociente es:", cociente)
+
+
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+residuo = numero1 % numero2
+print("El residuo es:", residuo)
+
+
+largo = float(input("Ingrese el largo del rectángulo: "))
+alto = float(input("Ingrese el alto del rectángulo: "))
+perimetro = 2 * (largo + alto)
+print("El perímetro del rectángulo es:", perimetro)
+
+"""INICIO
+Escribir numero
+Leer numero 
+doble <- numero*2
+Escribir "El Doble es:", doble
+Fin"""
